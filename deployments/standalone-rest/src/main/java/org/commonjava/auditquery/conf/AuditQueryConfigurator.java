f6df213f1933f144f6dcb1c1f9a9ce27b2403e32
Wenjie Guo <wguo@redhat.com>
2018-12-04 14:55:29 +0800

    Setup the basic stuff for configuration-reading

---
+++ deployments/standalone-rest/src/main/java/org/commonjava/auditquery/conf/AuditQueryConfigurator.java
@@ -0,0 +1,56 @@
+/**
+ * Copyright (C) 2011-2018 Red Hat, Inc. (https://github.com/Commonjava/auditquery)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.commonjava.auditquery.conf;
+
+import org.commonjava.propulsor.boot.BootOptions;
+import org.commonjava.propulsor.config.ConfigurationException;
+import org.commonjava.propulsor.config.Configurator;
+import org.commonjava.propulsor.config.ConfiguratorException;
+import org.commonjava.propulsor.config.dotconf.DotConfConfigurationReader;
+import org.commonjava.propulsor.config.io.ConfigFileUtils;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+
+@ApplicationScoped
+public class AuditQueryConfigurator
+                implements Configurator
+{
+
+    @Inject
+    DotConfConfigurationReader configurationReader;
+
+    @Override
+    public void load( BootOptions options ) throws ConfiguratorException
+    {
+        String config = options.getConfig();
+        File configFile = new File( System.getProperty( options.getHomeSystemProperty() ), config );
+
+        try (InputStream stream = ConfigFileUtils.readFileWithIncludes( configFile ))
+        {
+            configurationReader.loadConfiguration( stream );
+        }
+        catch ( ConfigurationException | IOException e )
+        {
+            throw new ConfiguratorException( "Failed to read configuration: %s. Reason: %s", e, config,
+                                             e.getMessage() );
+        }
+    }
+}
