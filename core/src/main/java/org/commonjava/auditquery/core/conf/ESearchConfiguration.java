9d5e2845e9b15ffd84ca9f631566411e4e54de23
Wenjie Guo <wguo@redhat.com>
2019-01-18 13:26:11 +0800

    Add index configuration

--- core/src/main/java/org/commonjava/auditquery/core/conf/ESearchConfiguration.java
+++ core/src/main/java/org/commonjava/auditquery/core/conf/ESearchConfiguration.java
@@ -19,6 +19,10 @@ public class ESearchConfiguration
 
     private Integer port;
 
+    private String trackingSummaryIndex;
+
+    private String fileEventIndex;
+
     public String getHost()
     {
         return host;
@@ -30,14 +34,21 @@ public class ESearchConfiguration
         this.host = host;
     }
 
-    public Integer getPort()
-    {
-        return port;
-    }
+    public Integer getPort() { return port; }
 
     @ConfigName( "port" )
     public void setPort( Integer port )
     {
         this.port = port;
     }
+
+    public String getTrackingSummaryIndex() { return trackingSummaryIndex; }
+
+    @ConfigName( "index.tracking_summary" )
+    public void setTrackingSummaryIndex( String trackingSummaryIndex ) { this.trackingSummaryIndex = trackingSummaryIndex; }
+
+    public String getFileEventIndex() { return fileEventIndex; }
+
+    @ConfigName( "index.file_event" )
+    public void setFileEventIndex( String fileEventIndex ) { this.fileEventIndex = fileEventIndex; }
 }
