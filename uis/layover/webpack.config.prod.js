3bea4a42513846ea799d0797ef7097f1243e9a7d
Gang Li <gli@redhat.com>
2019-04-16 14:18:54 +0800

    Use prod mode for react compile to minimize size of bundle.js

---
+++ uis/layover/webpack.config.prod.js
@@ -0,0 +1,28 @@
+const path = require('path');
+
+module.exports = 
+{
+    "mode": "production",
+    "entry": "./app/js/app.js",
+    "output": {
+        "path": path.resolve(__dirname, 'dist'),
+        "filename": "bundle.js"
+    },
+    "module": {
+        "rules": [
+            {
+                "test": /\.(js|jsx)$/,
+                "exclude": /node_modules/,
+                "loader": "babel-loader",
+                "options": {
+                  "plugins": ["@babel/plugin-proposal-class-properties"],
+                  "presets": ["@babel/preset-react"]
+                }
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+              }
+        ]
+    }
+}
