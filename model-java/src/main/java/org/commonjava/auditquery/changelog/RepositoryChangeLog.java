053e797e6ad1c21461832ad577a591a3894e13ba
Gang Li <gli@redhat.com>
2019-05-21 17:51:08 +0800

    Fix to add Externalize to changelog

--- model-java/src/main/java/org/commonjava/auditquery/changelog/RepositoryChangeLog.java
+++ model-java/src/main/java/org/commonjava/auditquery/changelog/RepositoryChangeLog.java
@@ -22,226 +22,257 @@ import org.hibernate.search.annotations.Index;
 import org.hibernate.search.annotations.Indexed;
 
 import javax.persistence.Entity;
+import java.io.Externalizable;
+import java.io.IOException;
+import java.io.ObjectInput;
+import java.io.ObjectOutput;
 import java.io.Serializable;
 import java.util.Date;
 
 @Entity
 @Indexed
 public class RepositoryChangeLog
-	implements Serializable
+        implements Serializable, Externalizable
 {
-	@JsonProperty
-	@Field( index = Index.YES, analyze = Analyze.NO )
-	private String storeKey;
+    @JsonProperty
+    @Field( index = Index.YES, analyze = Analyze.NO )
+    private String storeKey;
 
-	@JsonProperty
-	@Field( index = Index.YES, analyze = Analyze.NO )
-	private Date changeTime;
+    @JsonProperty
+    @Field( index = Index.YES, analyze = Analyze.NO )
+    private Date changeTime;
 
-	@JsonProperty
-	@Field( index = Index.YES, analyze = Analyze.NO )
-	private String version;
+    @JsonProperty
+    @Field( index = Index.YES, analyze = Analyze.NO )
+    private String version;
 
-	@JsonProperty
-	@Field( index = Index.YES, analyze = Analyze.NO )
-	private String summary;
+    @JsonProperty
+    @Field( index = Index.YES, analyze = Analyze.NO )
+    private String summary;
 
-	@JsonProperty
-	@Field( index = Index.YES, analyze = Analyze.NO )
-	private RepoChangeType changeType;
+    @JsonProperty
+    @Field( index = Index.YES, analyze = Analyze.NO )
+    private RepoChangeType changeType;
 
-	@JsonProperty
-	@Field( index = Index.YES, analyze = Analyze.NO )
-	private String user;
+    @JsonProperty
+    @Field( index = Index.YES, analyze = Analyze.NO )
+    private String user;
 
-	@JsonProperty
-	@Field( index = Index.YES, analyze = Analyze.NO )
-	private String diffContent;
+    @JsonProperty
+    @Field( index = Index.YES, analyze = Analyze.NO )
+    private String diffContent;
 
-	public String getStoreKey()
-	{
-		return storeKey;
-	}
+    public String getStoreKey()
+    {
+        return storeKey;
+    }
 
-	public void setStoreKey( String storeKey )
-	{
-		this.storeKey = storeKey;
-	}
+    public void setStoreKey( String storeKey )
+    {
+        this.storeKey = storeKey;
+    }
 
-	public Date getChangeTime()
-	{
-		return changeTime;
-	}
+    public Date getChangeTime()
+    {
+        return changeTime;
+    }
 
-	public void setChangeTime( Date changeTime )
-	{
-		this.changeTime = changeTime;
-	}
+    public void setChangeTime( Date changeTime )
+    {
+        this.changeTime = changeTime;
+    }
 
-	public String getVersion()
-	{
-		return version;
-	}
+    public String getVersion()
+    {
+        return version;
+    }
 
-	public void setVersion( String version )
-	{
-		this.version = version;
-	}
+    public void setVersion( String version )
+    {
+        this.version = version;
+    }
 
-	public String getSummary()
-	{
-		return summary;
-	}
+    public String getSummary()
+    {
+        return summary;
+    }
 
-	public void setSummary( String summary )
-	{
-		this.summary = summary;
-	}
+    public void setSummary( String summary )
+    {
+        this.summary = summary;
+    }
 
-	public RepoChangeType getChangeType()
-	{
-		return changeType;
-	}
+    public RepoChangeType getChangeType()
+    {
+        return changeType;
+    }
 
-	public void setChangeType( RepoChangeType changeType )
-	{
-		this.changeType = changeType;
-	}
+    public void setChangeType( RepoChangeType changeType )
+    {
+        this.changeType = changeType;
+    }
 
-	public String getUser()
-	{
-		return user;
-	}
+    public String getUser()
+    {
+        return user;
+    }
 
-	public void setUser( String user )
-	{
-		this.user = user;
-	}
+    public void setUser( String user )
+    {
+        this.user = user;
+    }
 
-	public String getDiffContent()
-	{
-		return diffContent;
-	}
+    public String getDiffContent()
+    {
+        return diffContent;
+    }
 
-	public void setDiffContent( String diffContent )
-	{
-		this.diffContent = diffContent;
-	}
+    public void setDiffContent( String diffContent )
+    {
+        this.diffContent = diffContent;
+    }
 
-	@Override
-	public int hashCode()
-	{
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ( ( storeKey == null ) ? 1 : storeKey.hashCode() );
-		result = prime * result + ( ( changeTime == null ) ? 2 : changeTime.hashCode() );
-		result = prime * result + ( ( version == null ) ? 3 : version.hashCode() );
-		result = prime * result + ( ( summary == null ) ? 5 : summary.hashCode() );
-		result = prime * result + ( ( changeType == null ) ? 8 : changeType.hashCode() );
-		result = prime * result + ( ( user == null ) ? 13 : user.hashCode() );
-		result = prime * result + ( ( diffContent == null ) ? 21 : diffContent.hashCode() );
-		return result;
-	}
+    @Override
+    public int hashCode()
+    {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ( ( storeKey == null ) ? 1 : storeKey.hashCode() );
+        result = prime * result + ( ( changeTime == null ) ? 2 : changeTime.hashCode() );
+        result = prime * result + ( ( version == null ) ? 3 : version.hashCode() );
+        result = prime * result + ( ( summary == null ) ? 5 : summary.hashCode() );
+        result = prime * result + ( ( changeType == null ) ? 8 : changeType.hashCode() );
+        result = prime * result + ( ( user == null ) ? 13 : user.hashCode() );
+        result = prime * result + ( ( diffContent == null ) ? 21 : diffContent.hashCode() );
+        return result;
+    }
 
-	@Override
-	public boolean equals( Object obj )
-	{
-		if ( this == obj )
-		{
-			return true;
-		}
-		if ( obj == null )
-		{
-			return false;
-		}
-		if ( getClass() != obj.getClass() )
-		{
-			return false;
-		}
-		final RepositoryChangeLog other = (RepositoryChangeLog) obj;
-		if ( storeKey == null )
-		{
-			if ( other.storeKey != null )
-			{
-				return false;
-			}
-		}
-		else if ( !storeKey.equals( other.storeKey ) )
-		{
-			return false;
-		}
-		if ( changeTime == null )
-		{
-			if ( other.changeTime != null )
-			{
-				return false;
-			}
-		}
-		else if ( !changeTime.equals( other.changeTime ) )
-		{
-			return false;
-		}
+    @Override
+    public boolean equals( Object obj )
+    {
+        if ( this == obj )
+        {
+            return true;
+        }
+        if ( obj == null )
+        {
+            return false;
+        }
+        if ( getClass() != obj.getClass() )
+        {
+            return false;
+        }
+        final RepositoryChangeLog other = (RepositoryChangeLog) obj;
+        if ( storeKey == null )
+        {
+            if ( other.storeKey != null )
+            {
+                return false;
+            }
+        }
+        else if ( !storeKey.equals( other.storeKey ) )
+        {
+            return false;
+        }
+        if ( changeTime == null )
+        {
+            if ( other.changeTime != null )
+            {
+                return false;
+            }
+        }
+        else if ( !changeTime.equals( other.changeTime ) )
+        {
+            return false;
+        }
 
-		if ( version == null )
-		{
-			if ( other.version != null )
-			{
-				return false;
-			}
-		}
-		else if ( !version.equals( other.version ) )
-		{
-			return false;
-		}
+        if ( version == null )
+        {
+            if ( other.version != null )
+            {
+                return false;
+            }
+        }
+        else if ( !version.equals( other.version ) )
+        {
+            return false;
+        }
 
-		if ( summary == null )
-		{
-			if ( other.summary != null )
-			{
-				return false;
-			}
-		}
-		else if ( !summary.equals( other.summary ) )
-		{
-			return false;
-		}
+        if ( summary == null )
+        {
+            if ( other.summary != null )
+            {
+                return false;
+            }
+        }
+        else if ( !summary.equals( other.summary ) )
+        {
+            return false;
+        }
 
-		if ( changeType == null )
-		{
-			if ( other.changeType != null )
-			{
-				return false;
-			}
-		}
-		else if ( !changeType.equals( other.changeType ) )
-		{
-			return false;
-		}
+        if ( changeType == null )
+        {
+            if ( other.changeType != null )
+            {
+                return false;
+            }
+        }
+        else if ( !changeType.equals( other.changeType ) )
+        {
+            return false;
+        }
 
-		if ( user == null )
-		{
-			if ( other.user != null )
-			{
-				return false;
-			}
-		}
-		else if ( !user.equals( other.user ) )
-		{
-			return false;
-		}
+        if ( user == null )
+        {
+            if ( other.user != null )
+            {
+                return false;
+            }
+        }
+        else if ( !user.equals( other.user ) )
+        {
+            return false;
+        }
 
-		if ( diffContent == null )
-		{
-			if ( other.diffContent != null )
-			{
-				return false;
-			}
-		}
-		else if ( !diffContent.equals( other.diffContent ) )
-		{
-			return false;
-		}
+        if ( diffContent == null )
+        {
+            if ( other.diffContent != null )
+            {
+                return false;
+            }
+        }
+        else if ( !diffContent.equals( other.diffContent ) )
+        {
+            return false;
+        }
 
-		return true;
-	}
+        return true;
+    }
+
+    @Override
+    public void readExternal( ObjectInput in )
+            throws IOException, ClassNotFoundException
+    {
+        this.storeKey = (String) in.readObject();
+        this.changeTime = (Date) in.readObject();
+        this.version = (String) in.readObject();
+        this.summary = (String) in.readObject();
+        this.changeType = (RepoChangeType) in.readObject();
+        this.user = (String) in.readObject();
+        this.diffContent = (String) in.readObject();
+    }
+
+    @Override
+    public void writeExternal( ObjectOutput out )
+            throws IOException
+    {
+        out.writeObject( this.storeKey );
+        out.writeObject( this.changeTime );
+        out.writeObject( this.version );
+        out.writeObject( this.summary );
+        out.writeObject( this.changeType );
+        out.writeObject( this.user );
+        out.writeObject( this.diffContent );
+    }
+
 }
